424006709db6fe5137aeba624abcf8e5a27e679b
Theo Kikilas <kikilas@ceid.upatras.gr>
2024-11-25 11:51:36 +0200

    Request Controller

--- Project/Program.cs
+++ Project/Program.cs
@@ -14,28 +14,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Add DbContext to the services
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Project"))); // Use connection string from appsettings.json
-
 // Register the ApiSettings configuration section from appsettings.json
 builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
 
 // Register HttpClient for API calls (this is crucial for CountryHelper)
 builder.Services.AddHttpClient();
-builder.Services.AddMemoryCache(); // Adds IMemoryCache
-builder.Services.AddSingleton<CustomMemoryCache>();
 
-// Register CountryHelper with HttpClient and configuration from ApiSettings
-builder.Services.AddScoped(sp =>
-{
-    var httpClient = sp.GetRequiredService<HttpClient>(); // Get HttpClient instance
-    var dbContext = sp.GetRequiredService<ApplicationDbContext>(); // Get DbContext instance
-    var apiSettings = builder.Configuration.GetSection("ApiSettings").Get<ApiSettings>(); // Get configuration values
-    var memoryCache = sp.GetRequiredService<CustomMemoryCache>(); // Get IMemoryCache instance
-
-    return new CountryHelper(httpClient, dbContext, apiSettings?.RestCountriesUrl, memoryCache); // Pass HttpClient and the API URL to CountryHelper
-});
 
 var app = builder.Build();
 
